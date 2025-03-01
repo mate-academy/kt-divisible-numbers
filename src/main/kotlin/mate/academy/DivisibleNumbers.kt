4b1b505aab3d1e5d91fad6385752b80c6fb0dab7
OD <sdenisenko055@gmail.com>
2025-03-01 13:45:00 +0200

    result

--- src/main/kotlin/mate/academy/DivisibleNumbers.kt
+++ src/main/kotlin/mate/academy/DivisibleNumbers.kt
@@ -4,5 +4,5 @@ package mate.academy
 // with only positive numbers that are evenly divisible by `divider`
 
 fun getDivisibleNumbers(numbers: IntArray, divider: Int): IntArray {
-
+    return numbers.filter { e -> e >= 0 }.filter { e -> e % divider == 0 }.toIntArray()
 }
